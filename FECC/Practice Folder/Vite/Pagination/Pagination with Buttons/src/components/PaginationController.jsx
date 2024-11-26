dec1282caa8a6cb5a2562c83beba23934a5c2de7
Nitesh <nikss26796@gmail.com>
2024-11-26 22:26:44 +0530

    Updated the pagination logic and add few css property.

--- FECC/Practice Folder/Vite/Pagination/Pagination with Buttons/src/components/PaginationController.jsx
+++ FECC/Practice Folder/Vite/Pagination/Pagination with Buttons/src/components/PaginationController.jsx
@@ -1,19 +1,40 @@
 import { useState } from "react";
 
-const PaginationController = ({ data, itemPerPage }) => {
+const PaginationController = ({ data, itemPerPage, updatePage }) => {
   const totalPages = Math.ceil(data.length / itemPerPage);
   // console.log(totalPages)
   return (
     <>
-      <div className="pagination-buttons">
-        {/* I have used the Array.from method to create a array of button which could be utilized in pagination.*/ }
-     {Array.from({length:totalPages},(_,i)=>(
-        <button key={i+1}>{i+1}</button>
-     ))}
+      <div
+        className="pagination-buttons"
+        style={{
+          width: "25%",
+          margin: "auto",
+          display: "flex",
+          gap: "10px",
+          paddingBottom: "20px",
+        }}
+      >
+        {/* I have used the Array.from method to create a array of button which could be utilized in pagination.*/}
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => updatePage(i + 1)}
+            style={{
+              padding: "10px",
+              margin: "auto",
+              width: "40px",
+              fontSize: "16px",
+              textAlign: "center",
+              fontWeight: "600",
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
       </div>
     </>
   );
 };
 
 export { PaginationController };
-
--- FECC/Practice Folder/Vite/Pagination/Pagination with Buttons/src/components/User.jsx
+++ FECC/Practice Folder/Vite/Pagination/Pagination with Buttons/src/components/User.jsx
@@ -1,49 +1,65 @@
-import { useState,useEffect } from "react";
-import axios from 'axios';
-import "./User.css"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./User.css";
 import { PaginationController } from "./PaginationController";
 
+const UserDetails = () => {
+  const API = "https://jsonplaceholder.typicode.com/comments";
+  let [totalData, setTotalData] = useState([]);
+  let [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 50;
 
-const UserDetails=()=>{
-    const API="https://jsonplaceholder.typicode.com/comments";
-let [totalData,setTotalData]=useState([]);
-const itemPerPage=50;
-
-    useEffect(()=>{
-        const FetchUserDetails= async()=>{
-            try {
-            let resp=await axios.get(API)
-            {/*The below code will throw an error when the server does not send the data from it end.And if there is any error then the code wont execute further. */}
-            if(resp.status!== 200){throw new Error("API Error: " );}
-            let fetchedData=resp.data;
-            setTotalData(fetchedData);
-            }catch(error){
-                console.warn("Something went wrong",error)
-            }
+  useEffect(() => {
+    const FetchUserDetails = async () => {
+      try {
+        let resp = await axios.get(API);
+        {
+          /*The below code will throw an error when the server does not send the data from it end.And if there is any error then the code wont execute further. */
         }
-        FetchUserDetails();
-    },[]);
+        if (resp.status !== 200) {
+          throw new Error("API Error: ");
+        }
+        let fetchedData = resp.data;
+        setTotalData(fetchedData);
+      } catch (error) {
+        console.warn("Something went wrong", error);
+      }
+    };
+    FetchUserDetails();
+  }, []);
 
-    return(
-        <>
-        <h1>User details!!!!</h1>
-        <div className="paginationControlBox">
-        <PaginationController data={totalData} itemPerPage={itemPerPage}/>
-        </div>
-        <div className="userDataDisplayBox">
-            {totalData.map((item,index)=>{
-                let {name,email,body}=item;
-                return (
-                    <div className="UserCardBox" key={index}>
-                        <p className="nameOverflowControl">{name}</p>
-                        <p>{email}</p>
-                        <p className="commentOverflowControl"><strong>Comment: </strong>{body}</p>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
-}
+  let startIndex = (currentPage - 1) * itemPerPage;
+  let lastIndex = startIndex + itemPerPage;
+  let dataPerPage = totalData.slice(startIndex, lastIndex);
 
-export {UserDetails}
+  return (
+    <>
+      <h1>User details!!!!</h1>
+      <div className="paginationControlBox">
+        <PaginationController
+          data={totalData}
+          itemPerPage={itemPerPage}
+          updatePage={setCurrentPage}
+        />
+      </div>
+      <div className="userDataDisplayBox">
+        {dataPerPage.map((item, index) => {
+          let { id, name, email, body } = item;
+          return (
+            <div className="UserCardBox" key={index}>
+              <h2>{id}</h2>
+              <p className="nameOverflowControl">{name}</p>
+              <p>{email}</p>
+              <p className="commentOverflowControl">
+                <strong>Comment: </strong>
+                {body}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export { UserDetails };
