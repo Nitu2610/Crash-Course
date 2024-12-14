f33d8c1a8ac7b478c1b5e12db85ed871e0bae9e7
Nitu2610 <nikss26796@gmail.com>
2024-12-14 18:40:15 +0530

    changed the logo pathway.

--- FECC/Project/Bewakoof Website/src/components/Headercomp/Main_Header/logo.jsx
+++ FECC/Project/Bewakoof Website/src/components/Headercomp/Main_Header/logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from './MainHeader-logo-bwkf.svg'
+import logo from './image/MainHeader-logo-bwkf.svg'
 
 const MainHeaderLogo = () => {
   return <img src={logo} alt="logo-image not found" width={'55%'} />;
