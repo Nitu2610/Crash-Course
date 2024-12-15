4f3eb00f07c1b5c98c71e29bafbc7631f8e72c5f
Nitu2610 <nikss26796@gmail.com>
2024-12-15 15:35:08 +0530

    added the offer code part of the section.

---
+++ FECC/Project/Bewakoof Website/src/components/Sections/S2_Offer_code.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import offercodeimage from './images/section_offer_code.jpg'
+
+const OfferCode=()=>{
+    return(
+        <div style={{ margin:'auto', width:'100%'}}>
+        <img src={offercodeimage} alt="" width='100%' />
+        </div>
+    )
+}
+
+export {OfferCode}
